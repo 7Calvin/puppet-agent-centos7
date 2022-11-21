469a465f45a50cbd8512f4aa19b6a61c2b19280a
Calvin <116037275+7Calvin@users.noreply.github.com>
2022-11-21 10:58:57 -0300

    puppet-agent-centos7.sh

---
+++ puppet-agent-centos7.sh
@@ -0,0 +1,26 @@
+echo "Iniciando a instalacao....."
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install https://yum.puppet.com/puppet-release-el-7.noarch.rpm
+yum install puppet-agent -y
+
+
+
+echo "Configurando a instalacao....."
+
+cat <<EOF >> /etc/tmp.conf
+[agent]
+    server = foreman.calvin.local
+    certname = $HOSTNAME
+    runinterval = 180
+    environment = production
+    listen = false
+    pluginsync = true
+    report = true
+EOF
+
+sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+service puppet restart
+/opt/puppetlabs/bin/puppet agent -t
+
+echo "Finalizado a instalacao"
